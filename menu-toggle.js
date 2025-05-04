6c6c501c500c85741ac43d8c705b9e4080402e47
Artur Fernandes <artursilva1272@gmail.com>
2025-05-04 17:24:12 -0300

    agenda pronta

--- menu-toggle.js
+++ menu-toggle.js
@@ -5,4 +5,15 @@ document.addEventListener("DOMContentLoaded", function() {
     menuToggle.addEventListener("click", function() {
         navLinks.classList.toggle("active");
     });
+    document.addEventListener("click", function(event) {
+        if (!navLinks.contains(event.target) && !menuToggle.contains(event.target)) {
+            navLinks.classList.remove("active");
+        }
+    });
+
+    navLinks.addEventListener("click", function(event) {
+        if (event.target.tagName === "A") {
+            navLinks.classList.remove("active");
+        }
+    });
 });
